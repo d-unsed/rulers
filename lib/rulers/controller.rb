7563d224f905e031f283568b19b60828e7656ead
Dmitry Gritsay <unseductable@gmail.com>
2014-04-25 22:02:34 +0300

    Add ERB templates rendering

--- lib/rulers/controller.rb
+++ lib/rulers/controller.rb
@@ -5,5 +5,11 @@ module Rulers
     def initialize(env)
       @env = env
     end
+
+    def render(view_name, locals = {})
+      template = File.read("app/views/#{view_name}.html.erb")
+      eruby = Erubis::Eruby.new(template)
+      eruby.result(locals.merge(env: env))
+    end
   end
 end
--- spec/rulers/controller_spec.rb
+++ spec/rulers/controller_spec.rb
@@ -1,7 +1,38 @@
 require 'spec_helper'
 
 describe Rulers::Controller do
-  subject { Rulers::Controller.new(nil) }
+  subject(:controller) { Rulers::Controller.new(nil) }
 
   it { should respond_to(:env) }
+
+  describe '#render' do
+    let(:eruby)     { double() }
+    let(:view_name) { 'index' }
+    let(:contents)  { 'Template contents' }
+    let(:locals)    { { local_variable: 'some_value' } }
+    let(:env)       { { env_variable: 'env_value' } }
+    let(:call!)     { controller.render(view_name, locals) }
+
+    before do
+      File.stub(:read).and_return('Template contents')
+      Erubis::Eruby.stub(:new).and_return(eruby)
+      controller.stub(:env).and_return(env)
+      eruby.stub(:result)
+    end
+
+    it 'reads a template' do
+      expect(File).to receive(:read).with('app/views/index.html.erb')
+      call!
+    end
+
+    it 'creates Erubis object with templates contents' do
+      expect(Erubis::Eruby).to receive(:new).with(contents)
+      call!
+    end
+
+    it 'renders template with local variables' do
+      expect(eruby).to receive(:result).with(locals.merge(env: env))
+      call!
+    end
+  end
 end
